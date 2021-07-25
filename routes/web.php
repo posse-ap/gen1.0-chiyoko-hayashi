f94bd27c0abfffa750640f3d5e1fc412b06c0864
chiyoko-hayashi <chiyoko.hayashi@harbors.sh>
2021-07-25 11:50:44 +0900

    p202~204 リスト5-14~16

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::post('hello/add', 'HelloController@create');
 // リスト5-13
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
+
+// リスト5-16
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
