6a91b12c0d70388db06a36d07c678b42ed9e476d
chiyoko-hayashi <chiyoko.hayashi@harbors.sh>
2021-07-28 16:33:54 +0900

    コントローラーを利用してクイズの一覧画面を表示

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/quiz', function () {
-    return view('quizy.index');
-});
+Route::get('/quiz', 'QuizyController@index');
