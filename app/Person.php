6ed87de0829ed98bd15d459fe5d330812ae1da08
chiyoko-hayashi <chiyoko.hayashi@harbors.sh>
2021-09-12 18:32:08 +0900

    p251~252 グルーバルスコープの作成

--- app/Person.php
+++ app/Person.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
@@ -14,5 +15,24 @@ class Person extends Model
     public function scopeNameEqual($query, $str)
     {
         return $query->where('name', $str);
+    }
+
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age','>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age','<=', $n);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('age', function (Builder $builder){
+            $builder->where('age', '>', 20);
+        });
     }
 }
