7c2c363c8a12829cd798683ef26c3801cccb6687
chiyoko-hayashi <chiyoko.hayashi@harbors.sh>
2021-10-27 23:54:10 +0900

    laravel-adminをやってみた

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,5 +12,11 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
+    $router->resource('manager/users', UserController::class);
+    $router->resource('manager/title', TitleController::class);
 
 });
+
+// php artisan make:migration create_staffs_table --create=staffs
+// php artisan vendor:publish --provider="Encore\Admin\AdminServiceProvider"
+// php artisan admin:make UserController --model=App\\User
