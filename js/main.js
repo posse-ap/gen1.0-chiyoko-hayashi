4ae193fe092a5d7ed0682de267f2372d34beb078
chiyoko-hayashi <chiyoko.hayashi@harbors.sh>
2021-11-28 04:13:28 +0900

    edit:削除ができるようにした

--- js/main.js
+++ js/main.js
@@ -5,10 +5,34 @@
         el: '#app',
         data: {
             todos: [
-                'task 1',
-                'task 2',
-                'task 3',
+                {
+                    title: 'task 1',
+                    isDone: false
+                },
+                {
+                    title: 'task 2',
+                    isDone: false
+                },
+                {
+                    title: 'task 3',
+                    isDone: true
+                },
             ]
+        },
+        methods: {
+            addItem: function() {
+                var item = {
+                    title: this.newItem,
+                    isDone: false
+                };
+                this.todos.push(item);
+                this.newItem = '';
+            },
+            deleteItem: function(index) {
+                if (confirm('are you sure?')) {
+                    this.todos.splice(index, 1);
+                }
+            }
         }
     })
 })();
